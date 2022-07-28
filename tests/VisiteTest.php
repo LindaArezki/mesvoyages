6ff48aaa3e4bf035c1f6994c5f09e7f6f6962150
LindaARZ <91089848+Linda1345657@users.noreply.github.com>
2022-07-28 15:10:08 +0200

    Phase 24 : tests (compléments).

--- tests/VisiteTest.php
+++ tests/VisiteTest.php
@@ -23,4 +23,16 @@ class VisiteTest extends TestCase{
         $visite->setDatecreation(new DateTime("2022-04-14"));
         $this->assertEquals("14/04/2022", $visite->getDatecreationString());
     }
+    
+    public function testAddEnvironnement(){
+        $environnement = new Environnement();
+        $environnement->setNom("plage");
+        $visite = new Visite();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementAvant = $visite->getEnvironnements()->count();
+        $visite->addEnvironnement($environnement);
+        $nbEnvironnementApres = $visite->getEnvironnements()->count();
+        $this->assertEquals($nbEnvironnementAvant, $nbEnvironnementApres, "ajout même environnement devrait échouer");
+    }    
 }
+
